0f41da3ce67372f5e6c0999040dc3649ef0380f1
Yuxin Zhu <zhuyxn@gmail.com>
2013-03-08 18:56:07 -0800

    added gitignore

--- quotable/quotes/models.py
+++ quotable/quotes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+    user = models.ForeignKey('account.QuotableUserProfile')
+    body = models.TextField()
+    sayer = models.CharField(max_length=100)
+    date_created = models.DatetimeField(auto_now=True)
+
+class Tag(models.Model):
+	text = models.CharField(max_length=100)
+
+
