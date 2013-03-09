1cec3ed7cbf500e7cd88324df6a6670d30917c68
Yuxin Zhu <zhuyxn@gmail.com>
2013-03-08 19:58:22 -0800

    added facebook button css

--- quotable/account/models.py
+++ quotable/account/models.py
@@ -4,3 +4,28 @@ from django_facebook.models import FacebookProfileModel
 
 class QuotableUserProfile(FacebookProfileModel):
     user = models.OneToOneField(User)
+
+def create_facebook_profile(sender, instance, created, **kwargs):
+    '''
+    Make sure a QuotableUserProfile is created when creating a
+    user.
+    '''
+    if created:
+        QuotableUserProfile.objects.create(user=instance)
+
+post_save.connect(create_facebook_profile, sender=User)
+
+def user_post_delete(sender, instance, **kwargs):
+    try:
+        BerekleytimeUserProfile.objects.get(user=instance).delete()
+    except:
+        pass
+
+def user_post_save(sender, instance, **kwargs):
+    try:
+        profile, new = QuotableUserProfile.objects.get_or_create(user=instance)
+    except:
+        pass
+
+models.signals.post_delete.connect(user_post_delete, sender=User)
+models.signals.post_save.connect(user_post_save, sender=User)
--- quotable/quotes/views.py
+++ quotable/quotes/views.py
@@ -1,4 +1,16 @@
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render_to_response, redirect
+from django.db import IntegrityError
+from django.template import RequestContext
+from django_facebook.api import FacebookUserConverter, get_persistent_graph
+from django.utils.simplejson import loads, dumps
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import password_change
+from django.contrib.auth import views as auth_views
+from django.http import HttpResponseRedirect
 
 def index(request):
 	pass
